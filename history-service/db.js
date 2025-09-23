b8c81a8068005c9d2f9a175d25a6afea3f5d8445
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-09-23 13:55:43 +0700

    update all code

--- history-service/db.js
+++ history-service/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: 5432,
-  user: postgres,
+  user: "postgres",  
   password: process.env.DB_PASSWORD,
-  database: wallet_history,
+  database: "wallet_history",
   ssl: {
     rejectUnauthorized: false   // bắt buộc để bỏ qua check cert khi connect RDS
   }
