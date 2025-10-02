41d458abe36a7c96d0514f4129c5c1bfa4eae6b3
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-10-02 18:01:48 +0700

    update api-gate service for test

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -3,7 +3,8 @@ const app = require('./API_Gateway')
 
 
 
-// Khởi động server auth-service
+
+// Khởi động server api-gateway lắng nghe trên cổng 3000
 app.listen(3000, () => {
   console.log('API Gateway listening on port 3000');
 });
